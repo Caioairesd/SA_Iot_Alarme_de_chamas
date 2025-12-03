19372cb3ccaddea724d2cdd68c6547206561ec44
Caio Vinicius <caioviniaires@gmail.com>
2025-12-02 21:32:11 -0300

    Feat
    declarar pinos do sensor, botão, LED e buzzer

--- codigo_sa.c
+++ codigo_sa.c
@@ -1,7 +1,12 @@
+const int fireSensorPin = 7;
+const int resetButtonPin = 6;
+const int ledPin = 8;
+const int buzzerPin = 13;
+
 void setup() {
-  // Configurações iniciais
+
 }
 
 void loop() {
-  // Loop principal
+
 }
